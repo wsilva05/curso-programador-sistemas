5c53696f84937f158fbdddadcbded67094dc695f
weslley.silva <wsilva05@gmail.com>
2019-08-17 10:02:39 -0300

    estou salvando nova linha no projeto

---
+++ prj_salvando_no_git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace prj_salvando_no_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine( "Teste");
+            Console.ReadLine();
+        }
+    }
+}
